f5f8fdf2c77755a5bddbb668b00e09d0cef6f8e1
Christine Cua <cloudycua@gmail.com>
2018-04-28 02:29:41 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,3 +6,16 @@ CREATE TABLE projects (
   start_date REAL,
   end_date REAL
 );
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  age INTEGER,
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER,
+);
